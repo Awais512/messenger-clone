bfe96d7b85515739469738cfd43883478a061290
awais512 <raza.awais25@gmail.com>
2023-05-20 12:35:19 +0100

    Custom Input Components

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React, { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Input from "@/app/components/inputs/Input";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+      //Register
+    }
+    if (variant === "LOGIN") {
+      //Signin
+    }
+  };
+
+  const socialActions = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input label="Email" register={register} id="email" errors={errors} />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
